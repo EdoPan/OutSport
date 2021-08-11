3249570c2efb343804499cb35fad033bf05edf12
Marco Silveri <marcosilveri32@gmail.com>
2021-08-11 19:57:15 +0200

    Aggiunta del sistema di login e signup tramite l'uso di Firebase

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import firebase from 'firebase/app';
+import 'firebase/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor( private router: Router ) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return new Promise( (resolve, reject)=>{
+      firebase.auth().onAuthStateChanged((user: firebase.User)=>{
+        if(user){
+          resolve(true); //Significa che rimuoviamo la guardia
+        }else{
+          resolve(false);
+          this.router.navigate( ['login'] );
+        }
+      });
+    });
+  }
+
+}
